5314ce2e029f6b7e0a47c70e85f28aec91b5a842
The-Coder-Kishor <talakishor47@gmail.com>
2025-03-16 03:50:25 +0530

    Vectorstore: Handles various possible files agnostically

--- moya/vectorstore/base_vectorstore.py
+++ moya/vectorstore/base_vectorstore.py
@@ -7,6 +7,8 @@ Defines an abstract class describing how to store and retrieve vectors in multip
 import abc
 from typing import List
 from langchain_core.documents.base import Document
+from langchain_community.document_loaders import PyPDFLoader, BSHTMLLoader, TextLoader, JSONLoader, CSVLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class BaseVectorstoreRepository(abc.ABC):
     """
@@ -40,3 +42,32 @@ class BaseVectorstoreRepository(abc.ABC):
         Load a vectorstore from the specified directory
         """
         pass
+
+    def splitter(self, docs, chunk_size: int, chunk_overlap: int) -> None:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+        splits = text_splitter.split_documents(docs)
+        self.add_vector(splits)
+
+
+    def load_file(self, path: str)-> None:
+        """
+        Load a file into vectorstore
+        """
+        if not path:
+            raise ValueError("path is required")
+        elif path.endswith(".pdf"):
+            loader = PyPDFLoader(path)
+        elif path.endswith(".html"):
+            loader = BSHTMLLoader(path)
+        elif path.endswith(".md"):
+            loader = TextLoader(path)
+        elif path.endswith(".txt"):
+            loader = TextLoader(path)
+        elif path.endswith(".json"):
+            loader = JSONLoader(file_path=path, text_content=False)
+        elif path.endswith(".csv"):
+            loader = CSVLoader(file_path=path)
+        else:
+            raise ValueError("File type not supported")
+        docs = loader.load()
+        self.splitter(docs, 1000, 200)
